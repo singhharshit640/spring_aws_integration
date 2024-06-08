9743c87123c274033faa43900f2f8ba8ab637ea5
Harshit <singhharshit640@gmail.com>
2024-06-09 00:36:22 +0530

    intial commit

---
+++ src/main/java/com/example/demo/web/DemoController.java
@@ -0,0 +1,31 @@
+package com.example.demo.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@Tag(name = "Demo Controller")
+public class DemoController {
+
+    /*
+     * To access swagger use "localhost:8080/swagger-ui/index.html"
+     */
+    @Operation(summary= "Ping Backend", description= "API to ping the backend to check whether backend is up and working fine.")
+    @GetMapping("/aws/ping")
+    public String hello(){
+        return "Everthing's working fine bro! Chillax! 🍻";
+    }
+
+    /*
+     * To access swagger use "localhost:8080/swagger-ui/index.html"
+     */
+    @Operation(summary= "Default endpoint", description= "API to ping the backend to check whether backend is up and working fine.")
+    @GetMapping("/")
+    public String defaultHello(){
+        return "Hi Harshit! Your demo application is running just fine! Also you just deployed your first backend to AWS cloud! Yay 🎉";
+    }
+    
+}
