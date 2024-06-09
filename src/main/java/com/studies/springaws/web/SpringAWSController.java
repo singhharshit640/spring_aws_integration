7f55b6f17390509fde8fa3893ad89b4a246414fc
Harshit <singhharshit640@gmail.com>
2024-06-09 13:13:06 +0530

    method names changed

--- src/main/java/com/studies/springaws/web/SpringAWSController.java
+++ src/main/java/com/studies/springaws/web/SpringAWSController.java
@@ -15,7 +15,7 @@ public class SpringAWSController {
      */
     @Operation(summary= "Ping Backend", description= "API to ping the backend to check whether backend is up and working fine.")
     @GetMapping("/aws/ping")
-    public String hello(){
+    public String ping(){
         return "Everthing's working fine bro! Chillax! 🍻";
     }
 
@@ -24,7 +24,7 @@ public class SpringAWSController {
      */
     @Operation(summary= "Default endpoint", description= "API to ping the backend to check whether backend is up and working fine.")
     @GetMapping("/")
-    public String defaultHello(){
+    public String helloHarshit(){
         return "Hi Harshit! Your demo application is running just fine! Also you just deployed your first backend to AWS cloud! Yay 🎉";
     }
     
